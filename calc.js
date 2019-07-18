00b77b0fd7ce53305a14a53d13901dee210c39c4
Julie Davison <davison.jules@gmail.com>
2019-07-17 18:29:02 -0700

    adjustments

--- calc.js
+++ calc.js
@@ -5,42 +5,42 @@ var operand2 = Number(process.argv[4]);
 
 var answer;
 
-// if (operator === "add") {
-//     answer = operand1 + operand2;
-// }
-// else if (operator === "subtract") {
-//     answer = operand1 - operand2;
-// }
-// else if (operator === "multiply") {
-//     answer = operand1 * operand2;
-// }
-// else if (operator === "divide") {
-//     answer = operand1 / operand2;
-// }
-// else if (operator === "remainder") {
-//     answer = operand1 % operand2;
-// }
-
-// console.log(answer);
-
-
-switch (operator) {
-    case 'add':
-        answer = operand1 + operand2;
-        break;
-    case 'subtract':
-        answer = operand1 - operand2;
-        break;
-    case 'multiply':
-        answer = operand1 * operand2;
-        break;
-    case 'divide':
-        answer = operand1 / operand2;
-        break;
-    case 'divide':
-        answer = operand1 / operand2;
-        break;
-
-    default:
-        console.log(answer);
+if (operator === "add") {
+    answer = operand1 + operand2;
 }
+else if (operator === "subtract") {
+    answer = operand1 - operand2;
+}
+else if (operator === "multiply") {
+    answer = operand1 * operand2;
+}
+else if (operator === "divide") {
+    answer = operand1 / operand2;
+}
+else if (operator === "remainder") {
+    answer = operand1 % operand2;
+}
+
+console.log(answer);
+
+
+// switch (operator) {
+//     case 'add':
+//         answer = operand1 + operand2;
+//         break;
+//     case 'subtract':
+//         answer = operand1 - operand2;
+//         break;
+//     case 'multiply':
+//         answer = operand1 * operand2;
+//         break;
+//     case 'divide':
+//         answer = operand1 / operand2;
+//         break;
+//     case 'divide':
+//         answer = operand1 / operand2;
+//         break;
+
+//     default:
+//         console.log(answer);
+// }
